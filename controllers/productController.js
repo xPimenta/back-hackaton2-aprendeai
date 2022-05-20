d95c10e977a00e60f60ae8d80927a42a17f7c0b0
Pimenta <mateuspimartins@hotmail.com>
2022-05-20 20:27:35 -0300

    categories and questions test

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,13 +2,13 @@ import dataBase from "../database.js";
 
 export async function getCategories(req, res){
     try{
-        const products = await dataBase.collection("categories").find().toArray();
+        const categories = await dataBase.collection("categories").find().toArray();
         if(!products){
             res.sendStatus(404);
             return;
         }
 
-        res.send(products);
+        res.send(categories);
     }
     catch(e){
         res.sendStatus(500);
@@ -17,8 +17,7 @@ export async function getCategories(req, res){
 
 export async function getQuestion(req, res){
     // Converting ID to be recognized by collection
-    const categ = (req.params.categ)
-    console.log(categ)
+    const categ = req.params.categ;
    
     try{
         const questions = await dataBase.collection("questions").find({category: categ});
