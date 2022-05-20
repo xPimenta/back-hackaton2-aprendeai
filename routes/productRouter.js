c9766d648b50dd00126a988a222f71f27373d0ec
Pimenta <mateuspimartins@hotmail.com>
2022-05-20 20:58:45 -0300

    test params

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getCategories, getQuestion } from "../controllers/productController.js";
 
 const productRouter = Router();
+
 productRouter.get("/categories", getCategories);
 productRouter.get("/questions/:categ", getQuestion);
 
